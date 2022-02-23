a8a2132da11eab18fa4699846a79b2bac9bdcfc2
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2022-02-23 11:14:20 +0100

    Update code

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+'use strict'
+
 const http = require('http')
 
-const server = http.createServer(function (req, res) {
-  res.end('PONG')
+const server = http.createServer((req, res) => {
+  res.end(`Answered by "${process.env.CC_APP_NAME}" to "${req.url}"`)
 })
-
 
 server.listen(process.env.PORT)
